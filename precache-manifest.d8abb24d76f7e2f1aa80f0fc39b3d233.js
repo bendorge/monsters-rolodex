a9f80e4b8c05c7088a3a7b0fa2985d2b9c53d27b
Ben Dorge <bendorge@gmail.com>
2019-10-06 21:52:19 -0500

    Updates

---
+++ precache-manifest.d8abb24d76f7e2f1aa80f0fc39b3d233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea169582aa14c3031e81a81ecb4fe9d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b17ad0f03872f68a1a1",
+    "url": "/monsters-rolodex/static/css/main.36cadf62.chunk.css"
+  },
+  {
+    "revision": "5fe7237698c5e0f93d8e",
+    "url": "/monsters-rolodex/static/js/2.7ed93505.chunk.js"
+  },
+  {
+    "revision": "2b17ad0f03872f68a1a1",
+    "url": "/monsters-rolodex/static/js/main.99d32eb7.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
